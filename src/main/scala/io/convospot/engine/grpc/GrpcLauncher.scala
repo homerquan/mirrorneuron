bda6dd02adfdb8c7ddf14641959ea95d91c183cf
Homer Quan <homer@Homers-MacBook-Pro.local>
2017-11-13 22:15:36 -0500

    a quick commit

--- src/main/scala/io/convospot/engine/grpc/GrpcLauncher.scala
+++ src/main/scala/io/convospot/engine/grpc/GrpcLauncher.scala
@@ -11,42 +11,6 @@ import org.apache.log4j._
 
 private[convospot] object GrpcLauncher {
   val log = Logger.getLogger(getClass().getName())
-  private class ConversationImpl extends ConversationGrpc.Conversation {
-    override def say(req: Request) = {
-      val reply = Response(message = "Hello " + req.message)
-      Future.successful(reply)
-    }
-
-    /*
-     * @param responseObserver an observer to receive the stream of previous messages.
-     * @return an observer to handle requested message/location pairs.
-     */
-    override def talk(responseObserver: StreamObserver[Response]): StreamObserver[Request] = {
-      new StreamObserver[Request]() {
-        override def onNext(note: Request): Unit = {
-        }
-
-        override def onError(t: Throwable): Unit = {
-        }
-
-        override def onCompleted(): Unit = {
-          responseObserver.onCompleted
-        }
-      }
-    }
-
-    override def ask(req: Request) = {
-      req.`type` match {
-        case "create_a_new_bot" => {
-          Handlers.createBot(req)
-        }
-        case _ => {
-          val ex = new RuntimeException("unsupported request topic:"+ req.`type`)
-          Future.failed(ex)
-        }
-      }
-    }
-  }
 
   val server = new GrpcServer(
     ServerBuilder
